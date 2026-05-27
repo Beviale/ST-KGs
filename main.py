c3c95ecba989209cfd0efb8eeb423fd06abc3627
Alessandro Bevilacqua <beviale@outlook.it>
2026-05-27 23:22:06 +0200

    Update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  
 import sys
 from pathlib import Path
 sys.path.append(str(Path.cwd().parent))
@@ -8,6 +10,7 @@ from pykeen.triples import TriplesFactory
 from rdflib import Graph
 import os
 from pykeen.pipeline import pipeline
+from pykeen.hpo import hpo_pipeline
 from pykeen.evaluation import RankBasedEvaluator
 from pykeen.evaluation import InconsistentEvaluator
 from pykeen.evaluation import InconsistencyMetric
@@ -19,6 +22,9 @@ import pandas as pd
 from rdflib import Graph, URIRef
 import random
 from jdex.loaders.torch import KnowledgeGraph
+import shutil
+import subprocess
+import itertools
 
 def nt_to_tsv(input_path: str, output_path: str) -> None:
     """Convert a.nt file in a .tsv file."""
@@ -86,33 +92,219 @@ def select_dataset(base_path: str) -> str:
     return Path(os.path.join(base_path, datasets[choice]))
 
 
-def select_subgraph(graph_path: str, out_dir: None, ratio = 50):
-    if out_dir is None: 
-        p = Path(graph_path)
-        out_dir = p.parent
 
-    graph_triplets = parse_nt(graph_path)
-    print(f"[1/4] Reading the original file...")
-    print(f"      Total:  {len(graph_triplets):,} triplets")
+def train_TransE(dataset_path: str, entity_mapping, relation_mapping, experiments, output_directory):
+    train_tf = TriplesFactory.from_path(
+    dataset_path / pc.TRAIN,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
+    valid_tf = TriplesFactory.from_path(
+        dataset_path / pc.VALID,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
+    test_tf = TriplesFactory.from_path(
+        dataset_path / pc.TEST,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
 
- 
-    print(f"\n[2/4] Sampling triplets at {ratio*100:.0f}%...")
-    sampled = sample_graph(graph_triplets, ratio)
-    print(f"      Selected {len(sampled):,} triples on {len(graph_triplets):,}")
- 
-    print(f"\n[3/4] Computing entities and relations in sampled train...")
-    train_entities, train_relations = get_entities_and_relations(sampled)
-    print(f"      Entities:   {len(train_entities):,}")
-    print(f"      Relations: {len(train_relations):,}")
- 
-    print(f"\n[4/4] Saving to '{out_dir}/'...")
-    write_nt(sampled, out_dir / "sub_graph.nt")
- 
+    best_metric = -1.0  
+    best_pipeline_result = None
+    best_params = None
+    print(f"Number of hypeparameter combinations to test: {len(experiments)}\n")
+    for i, params in enumerate(experiments):
+        print(f"--- Experiment {i+1}/{len(experiments)} ---")
+        print(f"Current hyparameter values: {params}")
+
+        try:
+            result = pipeline(
+                model="TransE",
+                training=train_tf,
+                validation=valid_tf,
+                testing=test_tf,
+                optimizer="Adam",
+                optimizer_kwargs=dict(lr=params["lr"]),
+                model_kwargs=dict(embedding_dim=params["embedding_dim"]),
+                training_kwargs=100,
+                device="cuda",
+                evaluator=RankBasedEvaluator,  # Passi la classe
+                evaluator_kwargs=dict(
+                    metrics=["mrr", HitsAtK(k=1), HitsAtK(k=3), HitsAtK(k=10)],
+                    filtered=True,
+                ),
+            )
+            current_metric = result.metric_results.get_metric("hits@10")
+            print(f"Hits@10 obtained: {current_metric:.4f}")
+
+            if current_metric > best_metric:
+                best_metric = current_metric
+                best_pipeline_result = result
+                best_params = params
+                print("New BEST found!")
+
+        except Exception as e:
+            print(f"Error while experimenting with parameters {params}: {e}")
+            continue
+
+    print("\n" + "=" * 40)
+    print("GRID SEARCH COMPLETED")
+    print(f"Best Hits@10: {best_metric:.4f}")
+    print(f"Best hyparameter combination: {best_params}")
+    print("=" * 40)
+    print(result.metric_results.to_flat_dict())
+    os.makedirs(output_directory, exist_ok=True)
+    metrics_file_path = os.path.join(output_directory, "best_metrics_TransE.json")
+    with open(metrics_file_path, "w", encoding="utf-8") as f:
+        json.dump(result.metric_results, f, indent=4, sort_keys=True)
+
+    if best_pipeline_result is not None:
+        os.makedirs(os.path.join(output_directory, "Wights"), exist_ok=True)
+        best_pipeline_result.save_to_directory(output_directory)
+        print(f"\nBest model successfully saved in directory: '{output_directory}'")
+    return best_pipeline_result
+
+def evaluate_inc_best_model_TransE(ontology_path: str, train_path:str, output_kg_path: str, reasoner_path: str,  best_model, dataset_path: str, entity_mapping, relation_mapping, entity_to_id_path, relation_to_id_path, output_directory):
+    train_tf = TriplesFactory.from_path(
+    dataset_path / pc.TRAIN,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
+    valid_tf = TriplesFactory.from_path(
+        dataset_path / pc.VALID,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
+    test_tf = TriplesFactory.from_path(
+        dataset_path / pc.TEST,
+        entity_to_id=entity_mapping,
+        relation_to_id=relation_mapping,
+    )
+    for k in [1, 3, 10]:
+        inc_evaluator = InconsistentEvaluator(ontology_path, train_path, output_kg_path, reasoner_path, InconsistencyMetric.INC_AT_K, k, filtered=True)
+        inc_results = inc_evaluator.evaluate(
+            model=best_model.model,
+            mapped_triples=test_tf.mapped_triples,
+            additional_filter_triples=[train_tf.mapped_triples, valid_tf.mapped_triples],
+        )
+        print(f"Inc@{k}: {str(inc_results.data)}")
+        metrics_file_path = os.path.join(output_directory, "Inconsistent_Metrics_TransE.txt")
+        with open(metrics_file_path, "w", encoding="utf-8") as f:
+            f.write(f"Inc@{k}: {inc_results.data}\n")
+
+        inc_evaluator = InconsistentEvaluator(ontology_path, train_path, output_kg_path, reasoner_path, entity_to_id_path, relation_to_id_path, InconsistencyMetric.SEM_AT_K, k, filtered=True)
+        inc_results = inc_evaluator.evaluate(
+            model=best_model.model,
+            mapped_triples=test_tf.mapped_triples,
+            additional_filter_triples=[train_tf.mapped_triples, valid_tf.mapped_triples],
+        )
+        print(f"Sem@{k}: {str(inc_results.data)}")
+        metrics_file_path = os.path.join(output_directory, "Inconsistent_Metrics_TransE.txt")
+        with open(metrics_file_path, "w", encoding="utf-8") as f:
+            f.write(f"Sem@{k}: {inc_results.data}\n")
+
+        inc_evaluator = InconsistentEvaluator(ontology_path, train_path, output_kg_path, reasoner_path, entity_to_id_path, relation_to_id_path, InconsistencyMetric.AC_AT_K_1, k, filtered=True)
+        inc_results = inc_evaluator.evaluate(
+            model=best_model.model,
+            mapped_triples=test_tf.mapped_triples,
+            additional_filter_triples=[train_tf.mapped_triples, valid_tf.mapped_triples],
+        )
+        print(f"Ac@{k}_1: {str(inc_results.data)}")
+        metrics_file_path = os.path.join(output_directory, "Inconsistent_Metrics_TransE.txt")
+        with open(metrics_file_path, "w", encoding="utf-8") as f:
+            f.write(f"Ac@{k}_1: {inc_results.data}\n")
+
+        inc_evaluator = InconsistentEvaluator(ontology_path, train_path, output_kg_path, reasoner_path, entity_to_id_path, relation_to_id_path, InconsistencyMetric.AC_AT_K_2, k, filtered=True)
+        inc_results = inc_evaluator.evaluate(
+            model=best_model.model,
+            mapped_triples=test_tf.mapped_triples,
+            additional_filter_triples=[train_tf.mapped_triples, valid_tf.mapped_triples],
+        )
+        print(f"Ac@{k}_2: {str(inc_results.data)}")
+        metrics_file_path = os.path.join(output_directory, "Inconsistent_Metrics_TransE.txt")
+        with open(metrics_file_path, "w", encoding="utf-8") as f:
+            f.write(f"Ac@{k}_2: {inc_results.data}\n")
+
+
+def train_BoxE(dataset_path: str, dataset_name: str):
+    dataset_dir = Path("Boxe") / "Datasets" / dataset_name
+    dataset_dir_multi = Path("Boxe") / "DatasetsMulti" / dataset_name
+    dataset_dir.mkdir(parents=True, exist_ok=True)
+    dataset_dir_multi.mkdir(parents=True, exist_ok=True)
+    train_tsv_path = dataset_path / "abox" / "splits" / "train.tsv"
+    train_txt_path = dataset_dir / "train.txt"
+    train_txt_path_multi = dataset_dir_multi / "train.txt"
+    if train_tsv_path.exists():
+        shutil.copy2(train_tsv_path, train_txt_path)
+        print(f"File successfully overwritten at: {train_txt_path}")
+        shutil.copy2(train_tsv_path, train_txt_path_multi)
+        print(f"File successfully overwritten at: {train_txt_path_multi}")
+    else:
+        print(f"Error: The source file '{train_tsv_path}' was not found.")
+
+    valid_tsv_path = dataset_path / "abox" / "splits" / "valid.tsv"
+    valid_txt_path = dataset_dir / "valid.txt"
+    valid_txt_path_multi = dataset_dir_multi / "valid.txt"
+    if valid_tsv_path.exists():
+        shutil.copy2(valid_tsv_path, valid_txt_path)
+        print(f"File successfully overwritten at: {valid_txt_path}")
+        shutil.copy2(valid_tsv_path, valid_txt_path_multi)
+        print(f"File successfully overwritten at: {valid_txt_path_multi}")
+    else:
+        print(f"Error: The source file '{valid_tsv_path}' was not found.")
+
+    test_tsv_path = dataset_path / "abox" / "splits" / "test.tsv"
+    test_txt_path = dataset_dir / "test.txt"
+    test_txt_path_multi = dataset_dir_multi / "test.txt"
+    if test_tsv_path.exists():
+        shutil.copy2(test_tsv_path, test_txt_path)
+        print(f"File successfully overwritten at: {test_txt_path}")
+        shutil.copy2(test_tsv_path, test_txt_path_multi)
+        print(f"File successfully overwritten at: {test_txt_path_multi}")
+    else:
+        print(f"Error: The source file '{test_tsv_path}' was not found.")
+
+    #Pre-processing
+    command = f"conda activate boxe && cd Boxe && python KBUtils.py"
+    result = subprocess.run(
+        command,
+        shell=True, 
+        text=True,
+        capture_output=False, 
+    )
+    if result.returncode == 0:
+        print("=== PREPROCESSING COMPLETED SUCCESSFULLY ===")
+    else:
+        print(
+        f"=== ERROR DURING PREPROCESSING (Exit Code: {result.returncode}) ==="
+    )
+    
 
 
 def main():
+    repeat = True
+    while(repeat):
+        model_selected = None
+        print("1. TransE")
+        print("2. BoxE")
+        model_selection = input("Select a model: ")
+        if model_selection == "1":
+            model_selected = "TransE"
+        elif model_selection == "2":
+             model_selected = "BoxE"
+        else:
+            print("Repeat the insertion")
+        if model_selected is None:
+            repeat = True
+        else: 
+            repeat = False
+
     dataset_path = select_dataset("datasets")
-    print(f"Dataset selected: {dataset_path}")
+    dataset_name = os.path.basename(os.path.normpath(dataset_path))
+    output_directory = f"results_{model_selected}_{dataset_name}"
+    os.makedirs(output_directory, exist_ok=True)
+    print(f"Dataset selected: {dataset_name}")
     create_tsv_files(dataset_path, True)
     kg = KnowledgeGraph(
         path=dataset_path
@@ -137,80 +329,24 @@ def main():
         relation_mapping = json.load(f)
 
 
-    train_tf = TriplesFactory.from_path(
-        dataset_path / pc.TRAIN,
-        entity_to_id=entity_mapping,
-        relation_to_id=relation_mapping,
-    )
-    valid_tf = TriplesFactory.from_path(
-        dataset_path / pc.VALID,
-        entity_to_id=entity_mapping,
-        relation_to_id=relation_mapping,
-    )
-
-    test_tf = TriplesFactory.from_path(
-        dataset_path / pc.TEST,
-        entity_to_id=entity_mapping,
-        relation_to_id=relation_mapping,
-    )
-
-    evaluator = RankBasedEvaluator(
-        metrics=["mrr", HitsAtK(k=10)],
-        filtered=True,
-    )
-
-    result = pipeline(
-        training=train_tf,
-        validation=valid_tf, 
-        testing=test_tf,        
-        model="TransE",
-        evaluator = evaluator,
-        model_kwargs=dict(
-            embedding_dim=50,
-        ),
-        optimizer="Adam",
-        optimizer_kwargs=dict(
-            lr=0.01,
-        ),
-        training_kwargs=dict(
-            num_epochs=100,  
-            batch_size=256,
-            checkpoint_name='transE-checkpoint.pt',   
-            checkpoint_directory=dataset_path /'checkpoints/',    
-            checkpoint_on_failure=True
-        ),
-        
-        stopper="early",
-        stopper_kwargs=dict(
-            frequency=5,       
-            patience=2,       
-            relative_delta=0.01,
-            metric="mrr",     
-        ),
-        evaluation_kwargs=dict(
-            batch_size=256,  
-            use_tqdm=True,
-        ),
-        
-        device="gpu",
-        random_seed=42,
-    )
-    print(result.metric_results.to_flat_dict())
+    grid_hyperparameters = {
+        "embedding_dim": [50, 100, 200],
+        "lr": [1e-2, 1e-3],
+    }
+    keys, values = zip(*grid_hyperparameters.items())
+    experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
     ontology_path = dataset_path / "ontology.owl"
     train_path = dataset_path / "abox" / "splits" / "train.nt"
     output_kg_path = dataset_path / "ont_train_graph.owl"
     reasoner_path = Path().absolute() / "reasoners"
-    class_assertsions_json_path = dataset_path / "abox" / "class_assertions.json"
-    relation_json_path =  dataset_path / "rbox" / "roles_domain_range.json"
-    inc_evaluator = InconsistentEvaluator(ontology_path, train_path, output_kg_path, reasoner_path, entity_to_id_path, relation_to_id_path, 
-            InconsistencyMetric.SEM_AT_K, k=5, class_assertsions_json_path=class_assertsions_json_path, relation_json_path=relation_json_path, filtered=True)
-    inc_results = inc_evaluator.evaluate(
-        model=result.model,
-        mapped_triples=test_tf.mapped_triples,
-        additional_filter_triples=[train_tf.mapped_triples, valid_tf.mapped_triples],
-    )
-    print(f"Inc@5: {str(inc_results.data)}")
 
+    if model_selected == "TransE":
+        best_model = train_TransE(dataset_path, entity_mapping, relation_mapping, experiments, output_directory)
+        evaluate_inc_best_model_TransE(ontology_path, train_path, output_kg_path, reasoner_path, best_model, entity_to_id_path, relation_to_id_path, output_directory)
+    elif model_selected == "BoxE":
+        train_BoxE(dataset_path, dataset_name)
+
+        
 
 
 def parse_nt(path: str) -> list[tuple[str, str, str]]:
@@ -253,6 +389,23 @@ def sample_graph(triples, ratio, seed):
     k = max(1, int(len(triples) * ratio))
     return rng.sample(triples, k)
 
+
+def execute_subprocess(script_path: str, working_dir: str):
+    try:
+        process = subprocess.run(
+        [sys.executable, str(script_path.resolve())],
+        cwd=working_dir,
+        check=True,          # Solleva eccezione se il codice di uscita != 0
+        capture_output=True, # Cattura output e errori
+        text=True            # Converte in stringhe
+    )
+        
+        print("Script finished successfully!")
+        print("Output:", process.stdout)
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running the script in {working_dir}: {e}")
+        print(f"Error output: {e.stderr}")
 
 if __name__ == "__main__":
     main()
--- pykeen/src/pykeen/evaluation/inconsistent_evaluator.py
+++ pykeen/src/pykeen/evaluation/inconsistent_evaluator.py
@@ -12,6 +12,7 @@ from rdflib import URIRef
 from xml.etree.ElementTree import iterparse
 from enum import Enum
 from rdflib import OWL, RDFS, RDF
+from jdex.loaders.torch import KnowledgeGraph
 
 class InconsistencyMetric(Enum):
     INC_AT_K = "inc_at_k"
@@ -39,10 +40,9 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
     
     metric_result_cls = InconsistentMetricResults
 
-    def __init__(self, ontology_path:str, train_path:str, output_kg_path:str, reasoner_path:str, entity_to_id_path:str, relation_to_id_path:str, metric:InconsistencyMetric, k=5, class_assertsions_json_path="", relation_json_path="", **kwargs):
+    def __init__(self, ontology_path:str, train_path:str, output_kg_path:str, reasoner_path:str, entity_to_id_path:str, relation_to_id_path:str, metric:InconsistencyMetric, kg: KnowledgeGraph, k, **kwargs):
         super().__init__(**kwargs)
-        self.class_assertions_json_path = class_assertsions_json_path
-        self.relation_json_path = relation_json_path
+        self.kg = kg
         self.ontology_path = ontology_path
         self.train_path = train_path
         self.output_kg_path = output_kg_path
@@ -121,7 +121,7 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
             for prediction in predictions:
                 inconsistent_for_triple = 0
                 for triple in prediction:
-                    if self._is_consistent_relation_json(target, triple, g) == False:
+                    if self._is_consistent_relation(target, triple, g) == False:
                         inconsistent_for_triple += 1
                 inc_percentage = inconsistent_for_triple / self.k
                 self.inconsistencies[target].append(inc_percentage)
@@ -131,7 +131,7 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
                 consistency_sum = 0.0
                 consistent_count = 0
                 for i, triple in enumerate(prediction):
-                    if self._is_consistent_relation_json(target, triple, g):
+                    if self._is_consistent_relation(target, triple, g):
                         consistent_count += 1
                         consistency_sum += consistent_count / (i + 1)
                 ac_at_k = consistency_sum / self.k
@@ -139,49 +139,18 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
 
 
 
-    def _is_consistent_relation_json(self, target, triple, g):
-        head = self.id_to_entity[triple[0].item()]
-        relation = self.id_to_relation[triple[1].item()]
-        relation_uri = rdflib.URIRef(relation)
-        tail = self.id_to_entity[triple[2].item()]
-        with open(self.class_assertions_json_path, "r", encoding="utf-8") as f:
-            class_assertions = json.load(f)
-        with open(self.relation_json_path, "r", encoding="utf-8") as f:
-            relation_assertions = json.load(f)
+    def _is_consistent_relation(self, target, triple):
+        head = self.kg.individual_to_id(triple[0].item())
+        relation = self.kg.obj_prop_to_id(triple[1].item())
+        tail = self.kg.individual_to_id(triple[2].item())
         if target == "head":
-            all_types_head = set(class_assertions[head])
-            relation_domains = self._get_domains(relation, relation_assertions)
+            all_types_head = self.kg.individual_classes(head).toList()
+            relation_domains = self.kg.obj_prop_domain(relation).toList()
             return bool(all_types_head.intersection(relation_domains))
         elif target == "tail":
-            all_types_tail = set(class_assertions[tail])
-            relation_ranges = self._get_domains(relation, relation_assertions)
+            all_types_tail = self.kg.individual_classes(tail).toList()
+            relation_ranges = self.kg.obj_prop_range(relation).toList()
             return bool(all_types_tail.intersection(relation_ranges))
-        
-    def _is_consistent_relation_owl(self, target, triple, g):
-        head = self.id_to_entity[triple[0].item()]
-        relation = self.id_to_relation[triple[1].item()]
-        relation_uri = rdflib.URIRef(relation)
-        tail = self.id_to_entity[triple[2].item()]
-       
-        if target == "head":
-            head_uri = rdflib.URIRef(head)
-            types_head = set(g.objects(subject=head_uri, predicate=RDF.type))
-            all_types_head = set()
-            for h in types_head:
-                all_types_head.add(h)
-                all_types_head |= self._get_all_superclasses(h, g)
-            relation_ranges = set(g.objects(subject=relation_uri, predicate=RDFS.domain))
-            return bool(all_types_head.intersection(relation_ranges))
-        elif target == "tail":
-            tail_uri = rdflib.URIRef(tail)
-            types_tail = set(g.objects(subject=tail_uri, predicate=RDF.type))
-            all_types_tail = set()
-            for t in types_tail:
-                all_types_tail.add(t)
-                all_types_tail |= self._get_all_superclasses(t, g)
-            relation_ranges = set(g.objects(subject=relation_uri, predicate=RDFS.range))
-            return bool(all_types_tail.intersection(relation_ranges))
-
 
 
 
@@ -221,7 +190,6 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
         data = {}
         for side in ["head", "tail"]:
             if self.inconsistencies[side]:
-                # media delle percentuali per query
                 inc_at_k = np.mean(self.inconsistencies[side])
             else:
                 inc_at_k = 0.0
@@ -231,7 +199,7 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
     
 
     def _uri_to_prefix_tag(self, uri: str) -> str:
-        """Converte URI in tag XML con namespace."""
+        """Convert an URI in a XML tag with namespace."""
         if "#" in uri:
             ns, local = uri.rsplit("#", 1)
             ns += "#"
@@ -243,30 +211,3 @@ class InconsistentEvaluator(Evaluator[InconsistentMetricKey]):
             return None
         return f"{prefix}:{local}"
 
-    def _get_all_superclasses(self, class_uri, g):
-        """Risale la gerarchia delle classi."""
-        superclasses = set()
-        for superclass in g.objects(subject=class_uri, predicate=RDFS.subClassOf):
-            superclasses.add(superclass)
-            superclasses |= self._get_all_superclasses(superclass, g)  # ricorsivo
-        return superclasses
-    
-    def _get_domains(self, relation_uri: str, relation_assertions: dict) -> set:
-            domains = set()
-            for domain in relation_assertions.get(relation_uri, {}).get("domain", []):
-                if isinstance(domain, str):
-                    domains.add(domain)
-                elif isinstance(domain, dict):
-                    for cls in domain.get("http://www.w3.org/2002/07/owl#unionOf", []):
-                        domains.add(cls)
-            return domains
-
-    def _get_ranges(self, relation_uri: str, relation_assertions: dict) -> set:
-        ranges = set()
-        for range_ in relation_assertions.get(relation_uri, {}).get("range", []):
-            if isinstance(range_, str):
-                ranges.add(range_)
-            elif isinstance(range_, dict):
-                for cls in range_.get("http://www.w3.org/2002/07/owl#unionOf", []):
-                    ranges.add(cls)
-        return ranges
